e67fe9b9cdd1d58fe39b7a0b8b4517d8d312ae88
antonioarmada <antonioarmada@gmail.com>
2024-10-03 12:03:10 -0300

    Pyglet corriendo y mostrando background

--- ard_master/main_master/main_master.ino
+++ ard_master/main_master/main_master.ino
@@ -11,8 +11,9 @@ MCP2515 mcp2515(3);
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Hola CAN Rx");
-  
+  //Serial.println("Hola CAN Rx");
+  Serial.print("001");
+
   mcp2515.reset();
   mcp2515.setBitrate(CAN_125KBPS);
   mcp2515.setNormalMode();
@@ -22,11 +23,11 @@ void setup() {
 
 void loop() {
   if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
-    Serial.print("ID Recibido: ");
-    Serial.println(canMsg.can_id); // Imprime el ID
+    //Serial.print("ID Recibido: ");
+    Serial.print(canMsg.can_id); // Imprime el ID
     
-    Serial.print("recibo: ");
-    Serial.println(canMsg.data[0]);
+    //Serial.print("recibo: ");
+    //Serial.println(canMsg.data[0]);
     
     if(canMsg.data[0]){
       digitalWrite(led,HIGH);
--- pc/main.py
+++ pc/main.py
@@ -1,11 +1,54 @@
-# read serial port
 import serial
 import time
-ser = serial.Serial('/dev/tty.usbserial-A50285BI', 115200)
+import yaml
+import pyglet
+from pyglet.window import Window
+from pyglet.image import load
+
+
+# read config.yaml
+with open("config.yaml") as f:
+    cfg = yaml.load(f, Loader=yaml.FullLoader)
+
+# set serial port settings
+no_serial = cfg['no_serial']    
+try:
+    if no_serial:
+        ser = None
+    else:
+        ser = serial.Serial(cfg['port'], cfg['baudrate'])
+except:
+    print("Serial port not found")
+    no_serial = True
+
 time.sleep(2)
-# read serial port
-while True:
-    if ser.inWaiting() > 0:
-        line = ser.readline()
-        print(line)
-        
+
+# Create a Pyglet window
+window = Window(width=cfg['resolution'][0], height=cfg['resolution'][1])
+window.set_fullscreen(cfg['fullscreen'])
+window.set_exclusive_mouse(cfg['exclusive_mouse'])
+
+# Load the image
+image = load('assets/background.jpg') 
+sprite = pyglet.sprite.Sprite(image)
+
+# Scale the sprite to fit the screen
+sprite.scale = min(window.width / sprite.width, window.height / sprite.height)
+
+@window.event
+def on_draw():
+    window.clear()
+    sprite.draw()
+
+def update(dt):
+    # Read serial data
+    if not no_serial:
+        if ser.inWaiting() > 0:
+            line = ser.readline()
+            print(line)
+
+# Schedule the update function
+pyglet.clock.schedule_interval(update, 1/60.0)
+
+# Run the Pyglet app
+pyglet.app.run()
